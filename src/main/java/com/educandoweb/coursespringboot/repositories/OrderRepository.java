66d74ea38feb90e0e5250ef36338b9a62aeeb0eb
Adriano <adriano.junior@ccc.ufcg.edu.br>
2023-01-19 17:26:11 -0300

    Feat: add findAll and findByID of the Order entity

---
+++ src/main/java/com/educandoweb/coursespringboot/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.educandoweb.coursespringboot.repositories;
+
+import com.educandoweb.coursespringboot.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
