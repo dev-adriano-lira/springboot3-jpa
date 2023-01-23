fed798100688be73b664d5e0e140a54d72426b9a
Adriano <adriano.junior@ccc.ufcg.edu.br>
2023-01-23 10:44:29 -0300

    Feat: add findAll and findById of Category entity

--- src/main/java/com/educandoweb/coursespringboot/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursespringboot/config/TestConfig.java
@@ -1,7 +1,9 @@
 package com.educandoweb.coursespringboot.config;
 
+import com.educandoweb.coursespringboot.entities.Category;
 import com.educandoweb.coursespringboot.entities.Order;
 import com.educandoweb.coursespringboot.entities.enums.OrderStatus;
+import com.educandoweb.coursespringboot.repositories.CategoryRepository;
 import com.educandoweb.coursespringboot.repositories.OrderRepository;
 import com.educandoweb.coursespringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -33,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
---
+++ src/main/java/com/educandoweb/coursespringboot/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.educandoweb.coursespringboot.services;
+
+import com.educandoweb.coursespringboot.entities.Category;
+import com.educandoweb.coursespringboot.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
